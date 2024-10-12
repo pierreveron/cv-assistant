8c3e92fb8e04a7b22ba04b63cac9203b648303eb
Pierre Véron <pierre.veron@outlook.fr>
2024-10-12 09:44:38 +0200

    feat: add tw- prefix to tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="tw-grid tw-grid-rows-[20px_1fr_20px] tw-items-center tw-justify-items-center tw-min-h-screen tw-p-8 tw-pb-20 tw-gap-16 sm:tw-p-20 tw-font-[family-name:var(--font-geist-sans)]">
+      <main className="tw-flex tw-flex-col tw-gap-8 tw-row-start-2 tw-items-center sm:tw-items-start">
         <Image
-          className="dark:invert"
+          className="dark:tw-invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
+        <ol className="tw-list-inside tw-list-decimal tw-text-sm tw-text-center sm:tw-text-left tw-font-[family-name:var(--font-geist-mono)]">
+          <li className="tw-mb-2">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            <code className="tw-bg-black/[.05] dark:tw-bg-white/[.06] tw-px-1 tw-py-0.5 tw-rounded tw-font-semibold">
               app/page.tsx
             </code>
             .
@@ -23,15 +23,15 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="tw-flex tw-gap-4 tw-items-center tw-flex-col sm:tw-flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="tw-rounded-full tw-border tw-border-solid tw-border-transparent tw-transition-colors tw-flex tw-items-center tw-justify-center tw-bg-foreground tw-text-background tw-gap-2 hover:tw-bg-[#383838] dark:hover:tw-bg-[#ccc] tw-text-sm sm:tw-text-base tw-h-10 sm:tw-h-12 tw-px-4 sm:tw-px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
+              className="dark:tw-invert"
               src="https://nextjs.org/icons/vercel.svg"
               alt="Vercel logomark"
               width={20}
@@ -40,7 +40,7 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="tw-rounded-full tw-border tw-border-solid tw-border-black/[.08] dark:tw-border-white/[.145] tw-transition-colors tw-flex tw-items-center tw-justify-center hover:tw-bg-[#f2f2f2] dark:hover:tw-bg-[#1a1a1a] hover:tw-border-transparent tw-text-sm sm:tw-text-base tw-h-10 sm:tw-h-12 tw-px-4 sm:tw-px-5 sm:tw-min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -49,9 +49,9 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="tw-row-start-3 tw-flex tw-gap-6 tw-flex-wrap tw-items-center tw-justify-center">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="tw-flex tw-items-center tw-gap-2 hover:tw-underline hover:tw-underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -66,7 +66,7 @@ export default function Home() {
           Learn
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="tw-flex tw-items-center tw-gap-2 hover:tw-underline hover:tw-underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -81,7 +81,7 @@ export default function Home() {
           Examples
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="tw-flex tw-items-center tw-gap-2 hover:tw-underline hover:tw-underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
