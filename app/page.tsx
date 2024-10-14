726f7f15e9a3126ca1d464bdb96288b90783a86b
Pierre Véron <pierre.veron@outlook.fr>
2024-10-14 18:38:37 +0200

    feat: add link to start new conversation

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { useChat } from "./providers/ChatProvider";
 import { useTheme } from "./providers/ThemeProvider";
 import { useSidebar } from "./providers/SidebarProvider";
 import classNames from "classnames";
+import Link from "next/link";
 
 export default function Home() {
   const { resetChat } = useChat();
@@ -85,6 +86,25 @@ export default function Home() {
               </svg>
             )}
           </button>
+          <Link href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-circle-plus"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+              <path d="M9 12h6" />
+              <path d="M12 9v6" />
+            </svg>
+          </Link>
         </div>
 
         <ModelSelector />
